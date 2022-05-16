3950b0d5d1036c4d81685cbef4c1b9336f0a92e3
Demerak <alexandre_poirier@outlook.com>
2022-05-15 21:46:24 -0400

    Started adding functionality. Currently, the user can place numbers on the display. The user can also use the cursor to place numbers at a specific index. The button DEL was also implemented.

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,12 +3,51 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText display;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        display = findViewById(R.id.display);
+        display.setShowSoftInputOnFocus(false);
+        display.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (getString(R.string.display).equals(display.getText().toString())) {
+                    display.setText("");
+                }
+            }
+        });
+    }
+
+    public void onClickValueBTN(View view) {
+        Button b = (Button)view;
+        int value = Integer.parseInt(b.getText().toString());
+        int cursorPosition = display.getSelectionStart();
+        String strPrev = display.getText().toString();
+        String strLeft = strPrev.substring(0, cursorPosition);
+        String strRight = strPrev.substring(cursorPosition);
+        display.setText(String.format("%s%s%s", strLeft, Integer.toString(value),strRight));
+        display.setSelection(cursorPosition+1);
+    }
+
+    public void onClickDel(View view) {
+        int cursorPosition = display.getSelectionStart();
+        String strPrev = display.getText().toString();
+        if (strPrev.length() != 0) {
+            String strLeft = strPrev.substring(0, cursorPosition-1);
+            String strRight = strPrev.substring(cursorPosition);
+            display.setText(String.format("%s%s", strLeft, strRight));
+            display.setSelection(cursorPosition-1);
+        }
     }
 }
