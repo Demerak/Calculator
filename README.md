792135685243a48a717f6214783765ffdd32b28f
Demerak <alexandre_poirier@outlook.com>
2022-05-25 20:43:39 -0400

    Added logic behind calculating simple math expression

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Android Studio Calculator
 ## About The Project :blue_book:
 
-I'm currently learning mobile app development using Android Studio. I decided to start by trying to replicate some of the features on my CASIO calculator.
+I'm currently learning mobile app development using Android Studio. I decided to start by trying to replicate some of the features on my CASIO calculator. Some buttons are currently not implemented but you can still do basic calculations like a simple addition, subtraction, division, and multiplication. 
+
+![](calc.PNG)
--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -8,14 +8,23 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.math.BigDecimal;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
 public class MainActivity extends AppCompatActivity {
 
     private EditText display;
+    private ScriptEngine engine;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        engine = new ScriptEngineManager().getEngineByName("rhino");
 
         display = findViewById(R.id.display);
         display.setShowSoftInputOnFocus(false);
@@ -42,7 +51,6 @@ public class MainActivity extends AppCompatActivity {
             display.setText(String.format("%s%s%s", strLeft, Integer.toString(value),strRight));
         }
         display.setSelection(cursorPosition+1);
-
     }
 
     public void onClickDel(View view) {
@@ -73,6 +81,20 @@ public class MainActivity extends AppCompatActivity {
             display.setText(String.format("%s%s%s", strLeft,op,strRight));
         }
         display.setSelection(cursorPosition+1);
+    }
+
+    public void onClickEval(View view) {
+        try {
+            String onScreen = display.getText().toString();
+            onScreen = onScreen.replace("×", "*");
+            onScreen = onScreen.replace("÷", "/");
+            BigDecimal decimal = new BigDecimal(engine.eval(onScreen).toString());
+            String result = decimal.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString();
+            display.setText(result);
+        } catch (ScriptException e) {
+            display.setText("Error");
+        }
+
     }
 
 }
